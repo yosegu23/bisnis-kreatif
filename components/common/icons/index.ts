06d40ef6f48a1ae1e7d71421c1bd7554c28aa30c
yosegu23 <36472632+yosegu23@users.noreply.github.com>
2024-01-01 20:16:00 +0700

    my changes

--- components/common/icons/index.ts
+++ components/common/icons/index.ts
@@ -1,9 +1,7 @@
 import Hamburger from "./hamburger";
-import ShoppingCartIcon from "./shoppingCart";
 import Close from "./close";
 
 export {
  Hamburger,
- ShoppingCartIcon,
  Close
 }
--- components/product/index.ts
+++ components/product/index.ts
@@ -1,11 +1,9 @@
 import Description from "./description";
 import Reviews from "./reviews";
-import ProductInfo from "./productInfo";
 
 export {
     Description,
     Reviews,
-    ProductInfo
 }
 
 
